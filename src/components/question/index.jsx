49d2c98a60d2f2cf3cf2a6c4a6282aa2aef8add6
Nato Szmedra <nato@producteca.com>
2021-10-04 00:03:16 -0300

    feat: more question styling

--- src/components/question/index.jsx
+++ src/components/question/index.jsx
@@ -4,17 +4,21 @@ import { Button } from "react-bootstrap";
 import "./index.css";
 
 const Option = ({ onOptionSelected, option }) =>
-  <Button className="btn btn-dark option" onClick={() => onOptionSelected(option)}>{_.toUpper(option)}</Button>
+  <Button className="btn btn-dark option-button col-md-12" onClick={() => onOptionSelected(option)}>
+    <span className="option-text">{_.toUpper(option)}</span>
+  </Button>
 
 export default ({ question, options, onOptionSelected }) => (
   <div>
     <p>{question.text}</p>
-    {options.map((option, number) =>
-      <Option
-        key={number}
-        question={question}
-        option={option}
-        onOptionSelected={onOptionSelected}
-      /> )}
+    <div className="col-md-8">
+      {options.map((option, number) =>
+        <Option
+          key={number}
+          question={question}
+          option={option}
+          onOptionSelected={onOptionSelected}
+        /> )}
+    </div>
   </div>
 );
